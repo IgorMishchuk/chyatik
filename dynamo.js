7d00e83306c27c8bf1399c1154a4242f44ea001d
Igor Mishchuk <37332499+IgorMishchuk@users.noreply.github.com>
2018-09-19 14:49:47 +0300

    Update dynamo.js

--- dynamo.js
+++ dynamo.js
@@ -14,20 +14,20 @@ module.exports = {
             TableName : process.env.DB,
             KeyConditionExpression : "#i = :name",
             ScanIndexForward : false,       //Descending sort order
-            Limit : 1,                      //Show only first result, thus returning only latest entry in DN for specified partition key.
+            Limit : 1,                      //Show only first result, thus returning only latest entry in DB for specified partition key.
         };
         dynamo.query(dynamo_params, function(err, data) {
             if (err || data.Count == 0) {   //If DB does not have any entry for specified partition key
                 if (data.Count == 0) {
                     var new_date_entry = new Date(event.message.date*1000).toISOString(); //Telegram gives date in epoch seconds, AWS processes in epoch milliseconds, thus multiplication by 1000
-                    var put_params = {
+                    var put_params = {  //Specify DynamoDB PutItem parameters
                         TableName: process.env.DB,
                         Item: {
                             'Dname': {S: 'Enot'},
                             'Date': {S: new_date_entry},
                         }
                     };
-                    dynamo.putItem(put_params, function (err, data) {
+                    dynamo.putItem(put_params, function (err, data) {   //Execute DynamoDB PutItem operation
                         if (err) {
                             console.log(err);
                         }
@@ -35,13 +35,12 @@ module.exports = {
                             console.log('Item put to DB');
                         }
                     });
-                    var post_data = querystring.stringify({
+                    var post_data = querystring.stringify({ //Build message for Telegram API
             	        'chat_id': event.message.chat.id,
-        	            'text': 'Поздравляю, Енотик, это первая твоя запись в увлекательном отслеживании периодичности твоих набегов на керамического друга.'
+        	            'text': 'Text for first trigger message'
                     });
 
-                    // Build the post options
-                    var post_options = {
+                    var post_options = {    //Create Telegram post options
                         hostname: 'api.telegram.org',
                         port: 443,
                         path: '/bot'+process.env.BOT_API_KEY+'/sendMessage', //Bot API key specified in Lambda environment variables
@@ -51,10 +50,9 @@ module.exports = {
                             'Content-Length': post_data.length
                         }
                     };
-
-                    // Create the post request
+                    
                     var body = '';
-                    var post_req = https.request(post_options, function(res) {
+                    var post_req = https.request(post_options, function(res) {  //Create https request to Telegram API
                         res.setEncoding('utf8');
 
                          // Save the returning data
@@ -77,7 +75,7 @@ module.exports = {
 
                     // Post the data
                     console.log("write the post");
-                    post_req.write(post_data);
+                    post_req.write(post_data);  //Post data to Telegram API
                     post_req.end();
                 }
                 else {
@@ -104,12 +102,10 @@ module.exports = {
                 //Calculate time diff
                 var diff = now_epoch - db_epoch; //Calculate time difference between message and latest entry n DB, in milliseconds
                 ////////////////////
-                var d_names = new Array("Воскресенье", "Понедельник", "Вторник", "Среду", "Четверг", "Пятницу", "Субботу"); //Array of week days
+                var d_names = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"); //Array of week days
                 var curr_day = db_time.getDay(); //Get day number in a week
-                var day_separator = curr_day == 2 ? 'во' : 'в'; //Tuesday requires different prepending word
-                ///////////////////
                 var curr_date = db_time.getDate(); //Get day number in month
-                var m_names = new Array("Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"); //Array of months
+                var m_names = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"); //Array of months
                 var curr_month = db_time.getMonth(); //Get month number
                 var db_hours = db_time.getHours(); //Get hours
                 hour_zero = db_hours < 10 ? '0' : ''; //If hour is <10, prepend 0
@@ -121,100 +117,16 @@ module.exports = {
                 //Display time diff
                 var total_seconds = diff / 1000; //Calculate difference in seconds
                 var days = Math.floor(total_seconds / (60*60*24)); //Calculate number of whole days in difference
-                switch (days) {
-                    case 1:
-                    case 21:
-                    case 31:
-                      day_quantity = 'день';
-                      break;
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 22:
-                    case 23:
-                    case 24:
-                      day_quantity = 'дня';
-                      break;
-                    default:
-                      day_quantity = 'дней';
-                  }
+                day_quantity = days == 1 ? 'day' : 'days';  //Detect if we need singular or plural
                 var hours_leftover = total_seconds % (60*60*24); //Calculate remaining hours
                 var hours = Math.floor(hours_leftover / (60*60)); //Calculate number of whole hours in difference
-                switch (hours) {
-                    case 1:
-                    case 21:
-                      hour_quantity = 'час';
-                      break;
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 22:
-                    case 23:
-                      hour_quantity = 'часа';
-                      break;
-                    default: 
-                      hour_quantity = 'часов';
-                  }
+                hour_quantity = hours == 1 ? 'hour' : 'hours';  //Detect if we need singular or plural
                 var minutes_leftover = hours_leftover % (60*60); //Calculate remaining minutes
                 var minutes = Math.floor(minutes_leftover / 60); //Calculate number of whole minutes in difference
-                switch (minutes) {
-                    case 1:
-                    case 21:
-                    case 31:
-                    case 41:
-                    case 51:
-                      minute_quantity = 'минуту';
-                      break;
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 22:
-                    case 23:
-                    case 24:
-                    case 32:
-                    case 33:
-                    case 34:
-                    case 42:
-                    case 43:
-                    case 44:
-                    case 52:
-                    case 53:
-                    case 54:
-                      minute_quantity = 'минуты';
-                      break;
-                    default:
-                      minute_quantity = 'минут';
-                  }
+                minute_quantity = minutes == 1 ? 'minute' : 'minutes';
                 var seconds = Math.floor(minutes_leftover % 60); //Calculate number of whole seconds in difference
-                switch (seconds) {
-                    case 1:
-                    case 21:
-                    case 31:
-                    case 41:
-                    case 51:
-                      second_quantity = 'секунду';
-                      break;
-                    case 2:
-                    case 3:
-                    case 4:
-                    case 22:
-                    case 23:
-                    case 24:
-                    case 32:
-                    case 33:
-                    case 34:
-                    case 42:
-                    case 43:
-                    case 44:
-                    case 52:
-                    case 53:
-                    case 54:
-                      second_quantity = 'секунды';
-                      break;
-                    default:
-                      second_quantity = 'секунд';
-                  }
-                var phrase = 'Последнее событие было ' + days + ' ' + day_quantity + ' ' + hours + ' ' + hour_quantity + ' ' + minutes + ' ' + minute_quantity + ' ' + seconds + ' ' + second_quantity + ' тому назад, ' + day_separator + ' ' + d_names[curr_day] + ', ' + curr_date + ' ' + m_names[curr_month] + ' в ' + hour_zero + db_hours + ':' + minute_zero + db_minutes + ':' + second_zero + db_seconds;
+                second_quantity = seconds == 1 ? 'second' : 'seconds';
+                var phrase = 'Last occurence was ' + days + ' ' + day_quantity + ' ' + hours + ' ' + hour_quantity + ' ' + minutes + ' ' + minute_quantity + ' ' + seconds + ' ' + second_quantity + ' ago, ' + 'on ' + d_names[curr_day] + ', ' + curr_date + ' ' + m_names[curr_month] + ' at ' + hour_zero + db_hours + ':' + minute_zero + db_minutes + ':' + second_zero + db_seconds;
                 console.log(phrase);
                 var post_data = querystring.stringify({
             	    'chat_id': event.message.chat.id,
@@ -259,7 +171,7 @@ module.exports = {
                 console.log("write the post");
                 post_req.write(post_data);
                 post_req.end();
-            ////////
+                ////////
                 var new_date_entry = new Date(event.message.date*1000).toISOString(); //Convert telegram epoch time *1000 to ISO format
                 var put_params = {
                     TableName: process.env.DB,
